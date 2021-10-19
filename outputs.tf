498447d5edaeca8e8d588099adcd98426c5e8625
Jack Morris <rudolphjacksonm@gmail.com>
2021-10-19 03:19:19 +0100

    Support for ip_restrictions on function apps (#13)

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "tenant_id" {
   value       = azurerm_function_app.function.identity[0].tenant_id
   description = "Created Managed Identity Tenant ID"
 }
+
+output "allowed_ip_addresses" {
+  value       = var.allowed_ip_addresses
+  description = "IP addresses that are allowed to access the Acmebot UI."
+}
