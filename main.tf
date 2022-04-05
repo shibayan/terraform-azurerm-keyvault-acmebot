e391976ca0fe642722107e3510627664a54c9928
David J. M. Karlsen <david@davidkarlsen.com>
2022-04-05 09:50:03 +0200

    Be compatible with provider v3 (#29)
    Signed-off-by: David J. M. Karlsen <david@davidkarlsen.com>

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "azurerm_storage_account" "storage" {
-  name                      = var.storage_account_name
-  resource_group_name       = var.resource_group_name
-  location                  = var.location
-  account_kind              = "Storage"
-  account_tier              = "Standard"
-  account_replication_type  = "LRS"
-  enable_https_traffic_only = true
-  allow_blob_public_access  = false
-  min_tls_version           = "TLS1_2"
+  name                            = var.storage_account_name
+  resource_group_name             = var.resource_group_name
+  location                        = var.location
+  account_kind                    = "Storage"
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  enable_https_traffic_only       = true
+  allow_nested_items_to_be_public = false
+  min_tls_version                 = "TLS1_2"
 }
 
 resource "azurerm_app_service_plan" "serverfarm" {
