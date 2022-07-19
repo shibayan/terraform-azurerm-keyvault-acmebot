ba2093c9aa9887b0b6ecc7d5c85a77480c819d71
Tatsuro Shibamura <me@shibayan.jp>
2022-07-19 17:06:40 +0900

    Upgrade to Terraform Provider for Azure 3.0

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
 output "principal_id" {
-  value       = azurerm_function_app.function.identity[0].principal_id
+  value       = azurerm_windows_function_app.function.identity[0].principal_id
   description = "Created Managed Identity Principal ID"
 }
 
 output "tenant_id" {
-  value       = azurerm_function_app.function.identity[0].tenant_id
+  value       = azurerm_windows_function_app.function.identity[0].tenant_id
   description = "Created Managed Identity Tenant ID"
 }
 
