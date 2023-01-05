769f9eaafd3fb959b2a862bc6f05cb6432fd88d6
Bioinformatics @ Bejo Zaden <bioinformaticsbejo@users.noreply.github.com>
2023-01-05 11:53:53 +0100

    output names

--- outputs.tf
+++ outputs.tf
@@ -13,47 +13,47 @@ output "allowed_ip_addresses" {
   description = "IP addresses that are allowed to access the Acmebot UI."
 }
 
-output "" {
+output "vnet_subnet_int_id" {
   value       = azurerm_subnet.vnet_int.name
-  description = "VNET integration name"
+  description = "VNET Subnet Id for Integration"
 }
 
-output "" {
-  value       = azurerm_subnet.vnet_pe.name
-  description = "VNET Private endpoing name"
+output "vnet_subnet_pe_id" {
+  value       = azurerm_subnet.vnet_pe.id
+  description = "VNET Subnet Id for Private Endpoint"
 }
 
-output "" {
+output "private_endpoint_function_id" {
   value       = azurerm_private_endpoint.func-pe.id
-  description = "Private Endpoint Function ID"
+  description = "Private Endpoint Function Id"
 }
 
-output "" {
+output "private_endpoint_function_dns_configs" {
   value       = azurerm_private_endpoint.func-pe.custom_dns_configs
   description = "Private Endpoint Function Custom DNS Configs"
 }
 
-output "" {
+output "private_endpoint_storage_id" {
   value       = azurerm_private_endpoint.sto-pe.id
   description = "Private Endpoint Storage ID"
 }
 
-output "" {
+output "private_endpoint_storage_dns_configs" {
   value       = azurerm_private_endpoint.sto-pe.custom_dns_configs
   description = "Private Endpoint Storage Custom DNS Configs"
 }
 
-output "" {
+output "storage_id" {
   value       = azurerm_storage_account.storage.id
   description = "Storage ID"
 }
 
-output "" {
+output "function_id" {
   value       = azurerm_windows_function_app.function.id
   description = "Function ID"
 }
 
-output "" {
+output "function_default_hostname" {
   value       = azurerm_windows_function_app.function.default_hostname
   description = "Function Default Hostname"
 }
