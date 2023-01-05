15ca8aa34faa85c21250386b7419cb7030d964f3
Bioinformatics @ Bejo Zaden <bioinformaticsbejo@users.noreply.github.com>
2023-01-05 11:47:26 +0100

    outputs

--- outputs.tf
+++ outputs.tf
@@ -13,12 +13,47 @@ output "allowed_ip_addresses" {
   description = "IP addresses that are allowed to access the Acmebot UI."
 }
 
-azurerm_subnet.vnet_int.name
-azurerm_subnet.vnet_pe.name
-azurerm_private_endpoint.func-pe.id
-azurerm_private_endpoint.func-pe.custom_dns_configs
-azurerm_private_endpoint.sto-pe.id
-azurerm_private_endpoint.sto-pe.custom_dns_configs
-azurerm_storage_account.storage.id
-azurerm_windows_function_app.function.id
-azurerm_windows_function_app.function.default_hostname
+output "" {
+  value       = azurerm_subnet.vnet_int.name
+  description = "VNET integration name"
+}
+
+output "" {
+  value       = azurerm_subnet.vnet_pe.name
+  description = "VNET Private endpoing name"
+}
+
+output "" {
+  value       = azurerm_private_endpoint.func-pe.id
+  description = "Private Endpoint Function ID"
+}
+
+output "" {
+  value       = azurerm_private_endpoint.func-pe.custom_dns_configs
+  description = "Private Endpoint Function Custom DNS Configs"
+}
+
+output "" {
+  value       = azurerm_private_endpoint.sto-pe.id
+  description = "Private Endpoint Storage ID"
+}
+
+output "" {
+  value       = azurerm_private_endpoint.sto-pe.custom_dns_configs
+  description = "Private Endpoint Storage Custom DNS Configs"
+}
+
+output "" {
+  value       = azurerm_storage_account.storage.id
+  description = "Storage ID"
+}
+
+output "" {
+  value       = azurerm_windows_function_app.function.id
+  description = "Function ID"
+}
+
+output "" {
+  value       = azurerm_windows_function_app.function.default_hostname
+  description = "Function Default Hostname"
+}
