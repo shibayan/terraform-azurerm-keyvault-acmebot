63b58ccdeb53807c307711bf914f5c41563886a2
Bioinformatics @ Bejo Zaden <bioinformaticsbejo@users.noreply.github.com>
2023-01-06 15:09:19 +0100

    missing comma

--- variables.tf
+++ variables.tf
@@ -350,7 +350,7 @@ locals {
   function_ip_restrictions = {
     for l, w in concat(
       [for v in var.allowed_ip_addresses                  : {"ip_address" =    v, "virtual_network_subnet_id" = null}],
-      [for v in var.virtual_network_subnet_ids_pe         : {"ip_address" = null, "virtual_network_subnet_id" =    v}]
+      [for v in var.virtual_network_subnet_ids_pe         : {"ip_address" = null, "virtual_network_subnet_id" =    v}],
       [for v in var.virtual_network_subnet_ids_integration: {"ip_address" = null, "virtual_network_subnet_id" =    v}]
     ): l => w
   }
